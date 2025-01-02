044f2c0db2ed65dc0376865139c573b5de85e9f9
gamin <gamin@DESKTOP-T7MROPC>
2025-01-02 10:25:10 +0300

    Add project files.

---
+++ Adamasmaca1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Adamasmaca1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button27 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(485, 487);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 36);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "HATA SAYISI :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(672, 487);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 32);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "6";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft JhengHei UI", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label10.Location = new System.Drawing.Point(86, 66);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(474, 74);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "--------------------";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(123, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(414, 57);
+            this.label11.TabIndex = 36;
+            this.label11.Text = "ADAM ASMACA";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(552, 252);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 53);
+            this.textBox1.TabIndex = 37;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button27
+            // 
+            this.button27.BackColor = System.Drawing.Color.White;
+            this.button27.Location = new System.Drawing.Point(537, 325);
+            this.button27.Name = "button27";
+            this.button27.Size = new System.Drawing.Size(111, 41);
+            this.button27.TabIndex = 38;
+            this.button27.Text = "SORGULA";
+            this.button27.UseVisualStyleBackColor = false;
+            this.button27.Click += new System.EventHandler(this.button27_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 451);
+            this.pictureBox1.TabIndex = 39;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(500, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 25);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Score :";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(588, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 29);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(730, 616);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button27);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "<<<<<<<<<<<<<<";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button27;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Adamasmaca1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adamasmaca1
+{
+    
+
+    public partial class Form1 : Form
+    {
+        string[] iller = {
+            "Adana","Adiyaman","Afyonkarahisar","Agri","Aksaray","Amasya","Ankara","Artvin","Aydin","Balikesir","Bilecik","Bingol","Bitlis","Bolu","Burdur","Bursa","Canakkale","Cankiri",
+            "Corum","Denizli","Diyarbakir","Edirne","Elazig","Erzincan","Erzurum","Eskisehir","Gaziantep","Giresun","Gumushane","Hakkari","Hatay","Igdir","Isparta","Istanbul","Izmir",
+            "Kahramanmaras","Karabuk","Karaman","Kars","Kastamonu","Kayseri","Kirikkale","Kirklareli","Kirsehir","Kilisim","Kocaeli","Konya","Kutahya","Malatya","Manisa","Mardin",
+            "Mersin","Mugla","Mus","Nevsehir","Nigde","Ordu","Osmaniye","Rize","Sakarya","Samsun","Siirt","Sinop","Sivas","Tekirdag","Tokat","Trabzon","Tunceli","Sanlıurfa",
+            "Usak","Van","Yalova","Yozgat","Zonguldak"
+        };
+
+        private string secilenIl;
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+            Random r = new Random();
+            int index = r.Next(iller.Length);
+            secilenIl = iller[index].ToUpper(); // seçilen ili büyük harfle yaz
+            label10.Text = new string('-', secilenIl.Length); // seçilen ili - halinde yaz
+            
+        }
+
+        private void RastgeleIlGoster()
+        {
+            Random r = new Random();
+            int index = r.Next(iller.Length);
+            secilenIl = iller[index].ToUpper(); // yeni il seçiliyor
+            label10.Text = Gizle(secilenIl); // yeni kelimeyi şifrele
+            label2.Text = "6"; // kalan hak 6
+            hata = 0; // hata sayısını sıfırla
+            pictureBox1.Load("Resimler/0.png"); //  birinci fotoğraf
+        }
+
+
+        private string Gizle(string a)
+        {
+            return new string('-', a.Length); // seçilen ili - halinde yaz
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            string girilenHarf = textBox1.Text.ToUpper(); // girilen harf büyük olsun 
+
+            if (girilenHarf.Length == 1) // sadece bir harf girilsin
+            {
+                string yeniGizlenmisIl = ""; //güncellenmiş gizlenmiş il
+                bool harfBulundu = false; // Hhrarf kelimede var mı
+
+                for (int i = 0; i < secilenIl.Length; i++)
+                {
+                    if (secilenIl[i].ToString().ToUpper() == girilenHarf)
+                    {
+                        yeniGizlenmisIl += secilenIl[i]; // Ddoğru harfi ekle
+                        harfBulundu = true; // harf bulundu
+                    }
+                    else
+                    {
+                        yeniGizlenmisIl += label10.Text[i]; // gizli karakter kalsın
+                    }
+                }
+
+                label10.Text = yeniGizlenmisIl; // güncellenmiş gizlenmiş il
+
+                // kelime tamamlandıysa
+                if (label10.Text == secilenIl)
+                {
+                    MessageBox.Show("Tebrikler! Doğru bildiniz!"); // kullanıcıyı tebrik et
+                    if (int.TryParse(label4.Text, out int mevcutDeğer))
+                    {
+                        mevcutDeğer++;
+                        label4.Text = mevcutDeğer.ToString(); // yeni değeri label4'e yaz
+                    }
+                    RastgeleIlGoster(); // yeni kelimeyi seç
+                    return; // işlemi sonlandır
+                }
+
+                // harf bulunmadıysa
+                if (!harfBulundu)
+                {
+                    if (int.TryParse(label2.Text, out int kalanHak)) // label2 deki sayıyı kontrol et
+                    {
+                        kalanHak--; // kalan hakkı 1 azalt
+                        label2.Text = kalanHak.ToString(); // yeni kalan hakkı göster
+                        hata++; // hata sayısını 1 artır
+                        MessageBox.Show($"Yanlış harf! Kalan hak: {kalanHak}"); // kullanıcıya hak hakkında bilgi ver
+
+                        // resmi güncelle
+                        pictureBox1.Load($"Resimler/{hata}.png");
+
+                        // kalan hak sıfır olduğunda kaybettin mesajı göster
+                        if (kalanHak <= 0)
+                        {
+                            MessageBox.Show("Kaybettiniz!"); // kaybettiniz mesajı ver
+                            label4.Text = "0"; // Label4 ü 0 a dönsün
+                            RastgeleIlGoster(); // Yeni kelimeyi seç
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen sadece bir harf girin!"); // Hata mesajı ver
+            }
+
+            textBox1.Clear(); // TextBox ı temizle
+        }
+
+
+        int hata = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Load("Resimler/" + hata + ".png"); 
+            
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
